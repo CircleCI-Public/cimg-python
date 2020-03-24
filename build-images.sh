8d841cb5bce381f495386aeb60d80360eaa65ffc
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-23 20:58:18 -0400

    Respin Python v3.8.2 to enable node variant. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 3.7/Dockerfile -t cimg/python:3.7.7  -t cimg/python:3.7 .
-docker build --file 3.7/node/Dockerfile -t cimg/python:3.7.7-node  -t cimg/python:3.7-node .
+docker build --file 3.8/Dockerfile -t cimg/python:3.8.2  -t cimg/python:3.8 .
+docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.2-node  -t cimg/python:3.8-node .
