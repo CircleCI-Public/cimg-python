882d7dd4f5e5454ed6f67e1439f0e89c0643f53d
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-23 20:35:25 -0400

    Respin Python v2.7.17 to enable node variant. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 3.7/Dockerfile -t cimg/python:3.7.7  -t cimg/python:3.7 .
+docker build --file 2.7/Dockerfile -t cimg/python:2.7.17  -t cimg/python:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/python:2.7.17-node  -t cimg/python:2.7-node .
