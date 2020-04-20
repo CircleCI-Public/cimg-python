61b05ceb1f7088e10eaa9695963089f43b8fb60f
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-04-20 11:20:58 -0400

    Add v2.7.18. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 3.8/Dockerfile -t cimg/python:3.8.2  -t cimg/python:3.8 .
-docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.2-node  -t cimg/python:3.8-node .
+docker build --file 2.7/Dockerfile -t cimg/python:2.7.18  -t cimg/python:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/python:2.7.18-node  -t cimg/python:2.7-node .
