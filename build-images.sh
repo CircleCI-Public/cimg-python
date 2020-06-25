ae12a7ec67652c6ce3092c4c4c651045a7912518
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-06-25 12:06:01 -0400

    Pub: 2.7.18,3.5.9,3.6.9,3.7.7, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+docker build --file 2.7/Dockerfile -t cimg/python:2.7.18  -t cimg/python:2.7 .
+docker build --file 2.7/node/Dockerfile -t cimg/python:2.7.18-node  -t cimg/python:2.7-node .
+docker build --file 3.5/Dockerfile -t cimg/python:3.5.9  -t cimg/python:3.5 .
+docker build --file 3.5/node/Dockerfile -t cimg/python:3.5.9-node  -t cimg/python:3.5-node .
+docker build --file 3.6/Dockerfile -t cimg/python:3.6.9  -t cimg/python:3.6 .
+docker build --file 3.6/node/Dockerfile -t cimg/python:3.6.9-node  -t cimg/python:3.6-node .
+docker build --file 3.7/Dockerfile -t cimg/python:3.7.7  -t cimg/python:3.7 .
+docker build --file 3.7/node/Dockerfile -t cimg/python:3.7.7-node  -t cimg/python:3.7-node .
 docker build --file 3.8/Dockerfile -t cimg/python:3.8.3  -t cimg/python:3.8 .
 docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.3-node  -t cimg/python:3.8-node .
