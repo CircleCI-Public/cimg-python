02308437201bddc481eb9de7275f291bee467709
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-10-05 15:17:21 -0400

    Publish v3.9.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 3.8/Dockerfile -t cimg/python:3.8.6  -t cimg/python:3.8 .
-docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.6-node  -t cimg/python:3.8-node .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.0  -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.0-node  -t cimg/python:3.9-node .
