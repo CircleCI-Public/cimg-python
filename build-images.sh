92d17a28e584043124f34b6617605f184306d22b
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-02-16 13:26:24 -0500

    Publish v3.6.13 and v3.7.10. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 3.8/Dockerfile -t cimg/python:3.8.7  -t cimg/python:3.8 .
-docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.7-node  -t cimg/python:3.8-node .
-docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.7-browsers  -t cimg/python:3.8-browsers .
+docker build --file 3.6/Dockerfile -t cimg/python:3.6.13  -t cimg/python:3.6 .
+docker build --file 3.6/node/Dockerfile -t cimg/python:3.6.13-node  -t cimg/python:3.6-node .
+docker build --file 3.6/browsers/Dockerfile -t cimg/python:3.6.13-browsers  -t cimg/python:3.6-browsers .
+docker build --file 3.7/Dockerfile -t cimg/python:3.7.10  -t cimg/python:3.7 .
+docker build --file 3.7/node/Dockerfile -t cimg/python:3.7.10-node  -t cimg/python:3.7-node .
+docker build --file 3.7/browsers/Dockerfile -t cimg/python:3.7.10-browsers  -t cimg/python:3.7-browsers .
