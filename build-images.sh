dc8a77fb62a52055192d753633fdbd6ada576a0b
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-02-22 16:33:36 -0500

    Publish v3.8.8 and v3.9.2. [release] (#77)

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 3.6/Dockerfile -t cimg/python:3.6.13  -t cimg/python:3.6 .
-docker build --file 3.6/node/Dockerfile -t cimg/python:3.6.13-node  -t cimg/python:3.6-node .
-docker build --file 3.6/browsers/Dockerfile -t cimg/python:3.6.13-browsers  -t cimg/python:3.6-browsers .
-docker build --file 3.7/Dockerfile -t cimg/python:3.7.10  -t cimg/python:3.7 .
-docker build --file 3.7/node/Dockerfile -t cimg/python:3.7.10-node  -t cimg/python:3.7-node .
-docker build --file 3.7/browsers/Dockerfile -t cimg/python:3.7.10-browsers  -t cimg/python:3.7-browsers .
+docker build --file 3.8/Dockerfile -t cimg/python:3.8.8  -t cimg/python:3.8 .
+docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.8-node  -t cimg/python:3.8-node .
+docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.8-browsers  -t cimg/python:3.8-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.2  -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.2-node  -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.2-browsers  -t cimg/python:3.9-browsers .
