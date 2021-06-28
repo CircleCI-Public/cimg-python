2eadd5100ce1593b9617e580cea89bdf4c4d26ff
Adam Hartley <git@ahartley.com>
2021-06-28 11:22:34 +0100

    Publish v3.9.6. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 3.8/Dockerfile -t cimg/python:3.8.10  -t cimg/python:3.8 .
-docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.10-node  -t cimg/python:3.8-node .
-docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.10-browsers  -t cimg/python:3.8-browsers .
-docker build --file 3.9/Dockerfile -t cimg/python:3.9.5  -t cimg/python:3.9 .
-docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.5-node  -t cimg/python:3.9-node .
-docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.5-browsers  -t cimg/python:3.9-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.6  -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.6-node  -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.6-browsers  -t cimg/python:3.9-browsers .
