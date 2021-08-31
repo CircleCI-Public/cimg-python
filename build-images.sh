85a450255d3ae8d19186ae83c98a52aedd9af28b
Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>
2021-08-31 09:35:17 -0700

    Publish v3.9.7. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 3.8/Dockerfile -t cimg/python:3.8.12  -t cimg/python:3.8 .
-docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.12-node  -t cimg/python:3.8-node .
-docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.12-browsers  -t cimg/python:3.8-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.7  -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.7-node  -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.7-browsers  -t cimg/python:3.9-browsers .
