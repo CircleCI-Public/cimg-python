13aa157a596658ffb1aa8b16a9d2706bd94d2ef8
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2021-11-17 05:13:41 +0000

    Publish v3.9.9. [release] (#105)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 3.9/Dockerfile -t cimg/python:3.9.8  -t cimg/python:3.9 .
-docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.8-node  -t cimg/python:3.9-node .
-docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.8-browsers  -t cimg/python:3.9-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.9  -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.9-node  -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.9-browsers  -t cimg/python:3.9-browsers .
