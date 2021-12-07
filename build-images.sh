8cf14b8a9bc5b449c94aa436ef0d616d7f38c958
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-12-07 13:48:45 -0500

    Publish v3.10.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 3.9/Dockerfile -t cimg/python:3.9.9  -t cimg/python:3.9 .
-docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.9-node  -t cimg/python:3.9-node .
-docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.9-browsers  -t cimg/python:3.9-browsers .
-docker build --file 3.10/Dockerfile -t cimg/python:3.10.0  -t cimg/python:3.10 .
-docker build --file 3.10/node/Dockerfile -t cimg/python:3.10.0-node  -t cimg/python:3.10-node .
-docker build --file 3.10/browsers/Dockerfile -t cimg/python:3.10.0-browsers  -t cimg/python:3.10-browsers .
+docker build --file 3.10/Dockerfile -t cimg/python:3.10.1  -t cimg/python:3.10 .
+docker build --file 3.10/node/Dockerfile -t cimg/python:3.10.1-node  -t cimg/python:3.10-node .
+docker build --file 3.10/browsers/Dockerfile -t cimg/python:3.10.1-browsers  -t cimg/python:3.10-browsers .
