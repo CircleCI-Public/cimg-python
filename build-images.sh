86bb9fc84670c185167c2d824d6cf22ecb0e6f47
Jalexchen <jeff.chen@circleci.com>
2022-01-18 08:03:17 -0800

    Publish v3.9.10 and v3.10.2. [release] (#116)
    * Publish v3.9.10 and v3.10.2. [release]
    
    * Enable compile time optimizations
    
    Co-authored-by: Ricardo N Feliciano <FelicianoTech@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 3.10/Dockerfile -t cimg/python:3.10.1  -t cimg/python:3.10 .
-docker build --file 3.10/node/Dockerfile -t cimg/python:3.10.1-node  -t cimg/python:3.10-node .
-docker build --file 3.10/browsers/Dockerfile -t cimg/python:3.10.1-browsers  -t cimg/python:3.10-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.10  -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.10-node  -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.10-browsers  -t cimg/python:3.9-browsers .
+docker build --file 3.10/Dockerfile -t cimg/python:3.10.2  -t cimg/python:3.10 .
+docker build --file 3.10/node/Dockerfile -t cimg/python:3.10.2-node  -t cimg/python:3.10-node .
+docker build --file 3.10/browsers/Dockerfile -t cimg/python:3.10.2-browsers  -t cimg/python:3.10-browsers .
