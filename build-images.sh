cee8f1a7d1a5fb47edb46759dcbabfb55afe441d
Jalexchen <jeff.chen@circleci.com>
2022-03-18 19:30:34 -0700

    Publish v3.7.13, v3.8.13, and v3.9.11. [release] (#124)

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.9/Dockerfile -t cimg/python:3.9.10  -t cimg/python:3.9 .
-docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.10-node  -t cimg/python:3.9-node .
-docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.10-browsers  -t cimg/python:3.9-browsers .
-docker build --file 3.10/Dockerfile -t cimg/python:3.10.2  -t cimg/python:3.10 .
-docker build --file 3.10/node/Dockerfile -t cimg/python:3.10.2-node  -t cimg/python:3.10-node .
-docker build --file 3.10/browsers/Dockerfile -t cimg/python:3.10.2-browsers  -t cimg/python:3.10-browsers .
+docker build --file 3.7/Dockerfile -t cimg/python:3.7.13  -t cimg/python:3.7 .
+docker build --file 3.7/node/Dockerfile -t cimg/python:3.7.13-node  -t cimg/python:3.7-node .
+docker build --file 3.7/browsers/Dockerfile -t cimg/python:3.7.13-browsers  -t cimg/python:3.7-browsers .
+docker build --file 3.8/Dockerfile -t cimg/python:3.8.13  -t cimg/python:3.8 .
+docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.13-node  -t cimg/python:3.8-node .
+docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.13-browsers  -t cimg/python:3.8-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.11  -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.11-node  -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.11-browsers  -t cimg/python:3.9-browsers .
---
+++ push-images.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/python:3.7.13
+docker push cimg/python:3.7
+docker push cimg/python:3.7.13-node
+docker push cimg/python:3.7-node
+docker push cimg/python:3.7.13-browsers
+docker push cimg/python:3.7-browsers
+
+docker push cimg/python:3.8.13
+docker push cimg/python:3.8
+docker push cimg/python:3.8.13-node
+docker push cimg/python:3.8-node
+docker push cimg/python:3.8.13-browsers
+docker push cimg/python:3.8-browsers
+
+docker push cimg/python:3.9.11
+docker push cimg/python:3.9
+docker push cimg/python:3.9.11-node
+docker push cimg/python:3.9-node
+docker push cimg/python:3.9.11-browsers
+docker push cimg/python:3.9-browsers
