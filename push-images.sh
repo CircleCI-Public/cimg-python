d0899387b027723c4b8c9684882905a1e3ce5cb8
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-03-28 12:51:19 -0400

    Publish v3.10.4. [release] (#128)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/python:3.10.3
+docker push cimg/python:3.10.4
 docker push cimg/python:3.10
-docker push cimg/python:3.10.3-node
+docker push cimg/python:3.10.4-node
 docker push cimg/python:3.10-node
-docker push cimg/python:3.10.3-browsers
+docker push cimg/python:3.10.4-browsers
 docker push cimg/python:3.10-browsers
