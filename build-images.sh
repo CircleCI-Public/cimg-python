0f782dfa5919d926c055225848b97dc360cb534c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-06-24 10:02:48 -0700

    Publish v3.10.5. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.9/Dockerfile -t cimg/python:3.9.13 -t cimg/python:3.9 .
-docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.13-node -t cimg/python:3.9-node .
-docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.13-browsers -t cimg/python:3.9-browsers .
+docker build --file 3.10/Dockerfile -t cimg/python:3.10.5  -t cimg/python:3.10 .
+docker build --file 3.10/node/Dockerfile -t cimg/python:3.10.5-node  -t cimg/python:3.10-node .
+docker build --file 3.10/browsers/Dockerfile -t cimg/python:3.10.5-browsers  -t cimg/python:3.10-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/python:3.9
-docker push cimg/python:3.9.13
-docker push cimg/python:3.9-node
-docker push cimg/python:3.9.13-node
-docker push cimg/python:3.9-browsers
-docker push cimg/python:3.9.13-browsers
+
+docker push cimg/python:3.10.5
+docker push cimg/python:3.10
+docker push cimg/python:3.10.5-node
+docker push cimg/python:3.10-node
+docker push cimg/python:3.10.5-browsers
+docker push cimg/python:3.10-browsers
