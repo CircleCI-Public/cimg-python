821676a3e3d294a05af8f649268f23647349cd2d
Jalexchen <jeff.chen@circleci.com>
2022-08-04 15:09:08 -0700

    Publish v3.10.6. [release] (#141)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/python:3.10.5
 docker push cimg/python:3.10
-docker push cimg/python:3.10.5-node
+docker push cimg/python:3.10.6
 docker push cimg/python:3.10-node
-docker push cimg/python:3.10.5-browsers
+docker push cimg/python:3.10.6-node
 docker push cimg/python:3.10-browsers
+docker push cimg/python:3.10.6-browsers
