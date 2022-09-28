2ff77d333f6c3e87d01daeccc503fa07ae3beb20
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-09-27 21:08:27 -0400

    Publish v3.9.14. [release] (#149)
    For some reason v3.9.14 didn't publish in
    https://github.com/CircleCI-Public/cimg-python/pull/148 so running it
    again here.

--- push-images.sh
+++ push-images.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/python:3.7
-docker push cimg/python:3.7.14
-docker push cimg/python:3.7-node
-docker push cimg/python:3.7.14-node
-docker push cimg/python:3.7-browsers
-docker push cimg/python:3.7.14-browsers
-docker push cimg/python:3.8
-docker push cimg/python:3.8.14
-docker push cimg/python:3.8-node
-docker push cimg/python:3.8.14-node
-docker push cimg/python:3.8-browsers
-docker push cimg/python:3.8.14-browsers
 docker push cimg/python:3.9
 docker push cimg/python:3.9.14
 docker push cimg/python:3.9-node
 docker push cimg/python:3.9.14-node
 docker push cimg/python:3.9-browsers
 docker push cimg/python:3.9.14-browsers
-docker push cimg/python:3.10
-docker push cimg/python:3.10.7
-docker push cimg/python:3.10-node
-docker push cimg/python:3.10.7-node
-docker push cimg/python:3.10-browsers
-docker push cimg/python:3.10.7-browsers
