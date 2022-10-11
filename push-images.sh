2826aa683c2ae3f3775122bee60cbfd1a81fcd86
Jeff Chen <jeff.chen@circleci.com>
2022-10-11 09:40:07 -0700

    Publish v3.9.15. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/python:3.9
-docker push cimg/python:3.9.14
+docker push cimg/python:3.9.15
 docker push cimg/python:3.9-node
-docker push cimg/python:3.9.14-node
+docker push cimg/python:3.9.15-node
 docker push cimg/python:3.9-browsers
-docker push cimg/python:3.9.14-browsers
+docker push cimg/python:3.9.15-browsers
