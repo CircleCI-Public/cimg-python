9abd599b7973925754f021c4696bdf54f8700883
Jalexchen <jeff.chen@circleci.com>
2022-10-25 08:13:07 -0700

    Publish v3.9.15. [release] (#157)
    will need to wait for pyenv to update. additionally, will need an
    additional update to dependencies, which will be PR #158
    
    3.7.15
    3.8.15
    3.10.8
    
    all need to be included in this PR as well

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.9/Dockerfile -t cimg/python:3.9.14 -t cimg/python:3.9 .
-docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.14-node -t cimg/python:3.9-node .
-docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.14-browsers -t cimg/python:3.9-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.15 -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.15-node -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.15-browsers -t cimg/python:3.9-browsers .
