ab5a51409af3fcf2848fb3c9ddbd95b611afcacd
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2022-10-25 12:19:04 -0400

    Publish v3.8.15. [release] (#159)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.9/Dockerfile -t cimg/python:3.9.15 -t cimg/python:3.9 .
-docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.15-node -t cimg/python:3.9-node .
-docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.15-browsers -t cimg/python:3.9-browsers .
+docker build --file 3.8/Dockerfile -t cimg/python:3.8.15 -t cimg/python:3.8 .
+docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.15-node -t cimg/python:3.8-node .
+docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.15-browsers -t cimg/python:3.8-browsers .
