08cbe7cd759d774d2fe37279eda8cfc21cd129a6
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-27 03:51:28 -0400

    Respin v3.8.15 and v3.9.15. [release]

--- build-images.sh
+++ build-images.sh
@@ -4,3 +4,6 @@
 docker build --file 3.8/Dockerfile -t cimg/python:3.8.15 -t cimg/python:3.8 .
 docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.15-node -t cimg/python:3.8-node .
 docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.15-browsers -t cimg/python:3.8-browsers .
+docker build --file 3.9/Dockerfile -t cimg/python:3.9.15 -t cimg/python:3.9 .
+docker build --file 3.9/node/Dockerfile -t cimg/python:3.9.15-node -t cimg/python:3.9-node .
+docker build --file 3.9/browsers/Dockerfile -t cimg/python:3.9.15-browsers -t cimg/python:3.9-browsers .
--- push-images.sh
+++ push-images.sh
@@ -6,3 +6,9 @@ docker push cimg/python:3.8-node
 docker push cimg/python:3.8.15-node
 docker push cimg/python:3.8-browsers
 docker push cimg/python:3.8.15-browsers
+docker push cimg/python:3.9
+docker push cimg/python:3.9.15
+docker push cimg/python:3.9-node
+docker push cimg/python:3.9.15-node
+docker push cimg/python:3.9-browsers
+docker push cimg/python:3.9.15-browsers
