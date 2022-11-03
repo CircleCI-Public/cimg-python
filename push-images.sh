b7939765aee07a4da283f7fd9f0cf749c1179530
Jalexchen <jeff.chen@circleci.com>
2022-11-02 17:05:02 -0700

    Pub: 3.7.15,3.8.15,3.9.15,3.10.8, and more. [release] (#170)
    Respin due to CVE

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/python:3.8
+docker push cimg/python:3.8.15
+docker push cimg/python:3.8-node
+docker push cimg/python:3.8.15-node
+docker push cimg/python:3.8-browsers
+docker push cimg/python:3.8.15-browsers
+docker push cimg/python:3.9
+docker push cimg/python:3.9.15
+docker push cimg/python:3.9-node
+docker push cimg/python:3.9.15-node
+docker push cimg/python:3.9-browsers
+docker push cimg/python:3.9.15-browsers
 docker push cimg/python:3.7
 docker push cimg/python:3.7.15
 docker push cimg/python:3.7-node
