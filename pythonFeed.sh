0db232d6e5730102d608d5a00a5a9f71d515620f
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-04-11 08:23:56 -0500

    [DNM] Created script to automate python updates (#185)

---
+++ pythonFeed.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+if [ -f shared/automated-updates.sh ]; then
+  source shared/automated-updates.sh
+else
+  echo "Check if submodule was loaded; automated-updates.sh is missing"
+  exit 1
+fi
+
+LATEST_VERSION=0
+VERSION_LIST=$(pyenv install --list | sed 's/^  //' | grep '^[3]' | grep --invert-match 'dev\|a\|b')
+
+getPythonVersionLatest() {
+
+    LATEST_VERSION=$(echo "$VERSION_LIST" | tail -1)
+}
+
+processLastVersion() {
+    PROCESSED_LATEST_VERSION=$(echo "$LATEST_VERSION" | cut -f1,2 -d'.')
+    VERSION_LIST=$(echo "$VERSION_LIST" | grep -v "$PROCESSED_LATEST_VERSION")
+    generateVersions "$LATEST_VERSION"
+    generateSearchTerms "PYTHON_VERSION" "$majorMinor"/Dockerfile '\'
+      # shellcheck disable=SC2154
+    directoryCheck "$majorMinor" "$SEARCH_TERM"
+    if [[ $(eval echo $?) == 0 ]]; then
+        echo "$LATEST_VERSION is valid"
+        VERSIONS+=$LATEST_VERSION
+        VERSIONS+=' '
+    fi
+}
+
+while [ "${PROCESSED_LATEST_VERSION}" != "3.7" ]
+do
+    getPythonVersionLatest
+    processLastVersion
+done
+
+if [ "$VERSIONS" != "" ]; then
+    echo "generating cimg-python for versions: $VERSIONS"
+    ./shared/release.sh "$VERSIONS"
+else
+    echo "No changes"
+fi
