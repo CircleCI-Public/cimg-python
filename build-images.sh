c25f69014070c55699295228a5ade8d3180d6646
Adam Hartley <git@ahartley.com>
2023-07-18 22:13:04 +0100

    Test build

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 3.11/Dockerfile -t cimg/python:3.11.4 -t cimg/python:3.11 --platform linux/amd64 .
-docker build --file 3.11/node/Dockerfile -t cimg/python:3.11.4-node -t cimg/python:3.11-node --platform linux/amd64 .
-docker build --file 3.11/browsers/Dockerfile -t cimg/python:3.11.4-browsers -t cimg/python:3.11-browsers --platform linux/amd64 .
-docker build --file 3.10/Dockerfile -t cimg/python:3.10.12 -t cimg/python:3.10 --platform linux/amd64 .
-docker build --file 3.10/node/Dockerfile -t cimg/python:3.10.12-node -t cimg/python:3.10-node --platform linux/amd64 .
-docker build --file 3.10/browsers/Dockerfile -t cimg/python:3.10.12-browsers -t cimg/python:3.10-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 3.11/Dockerfile -t cimg/python:3.11.4 -t cimg/python:3.11 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 3.11/node/Dockerfile -t cimg/python:3.11.4-node -t cimg/python:3.11-node --push .
+docker buildx build --platform=linux/amd64 --file 3.11/browsers/Dockerfile -t cimg/python:3.11.4-browsers -t cimg/python:3.11-browsers --push .
