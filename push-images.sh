48853f2fde5c780c5bad9ab40aedb18ee285a842
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-08-25 15:13:41 +0100

    Pub: v3.11.5, v3.10.13, v3.9.18, and more. [release] (#210)
    Pub: v3.11.5, v3.10.13, v3.9.18, and more. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/python:3.11
-docker push cimg/python:3.11.4
-docker push cimg/python:3.11-node
-docker push cimg/python:3.11.4-node
-docker push cimg/python:3.11-browsers
-docker push cimg/python:3.11.4-browsers
-docker push cimg/python:3.10
-docker push cimg/python:3.10.12
-docker push cimg/python:3.10-node
-docker push cimg/python:3.10.12-node
-docker push cimg/python:3.10-browsers
-docker push cimg/python:3.10.12-browsers
+set -eo pipefail
