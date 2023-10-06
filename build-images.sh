1e1da5a3e89e000914764f07ea04d45af96775d8
Jeff Chen <jeff.chen@circleci.com>
2023-10-06 09:11:14 -0700

    Publish v3.11.6. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-set -eo pipefail
 
-docker context create cimg
-docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.11/Dockerfile -t cimg/python:3.11.5 -t cimg/python:3.11 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.11/node/Dockerfile -t cimg/python:3.11.5-node -t cimg/python:3.11-node --push .
-docker buildx build --platform=linux/amd64 --file 3.11/browsers/Dockerfile -t cimg/python:3.11.5-browsers -t cimg/python:3.11-browsers --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.10/Dockerfile -t cimg/python:3.10.13 -t cimg/python:3.10 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.10/node/Dockerfile -t cimg/python:3.10.13-node -t cimg/python:3.10-node --push .
-docker buildx build --platform=linux/amd64 --file 3.10/browsers/Dockerfile -t cimg/python:3.10.13-browsers -t cimg/python:3.10-browsers --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.9/Dockerfile -t cimg/python:3.9.18 -t cimg/python:3.9 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.9/node/Dockerfile -t cimg/python:3.9.18-node -t cimg/python:3.9-node --push .
-docker buildx build --platform=linux/amd64 --file 3.9/browsers/Dockerfile -t cimg/python:3.9.18-browsers -t cimg/python:3.9-browsers --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.8/Dockerfile -t cimg/python:3.8.18 -t cimg/python:3.8 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.8/node/Dockerfile -t cimg/python:3.8.18-node -t cimg/python:3.8-node --push .
-docker buildx build --platform=linux/amd64 --file 3.8/browsers/Dockerfile -t cimg/python:3.8.18-browsers -t cimg/python:3.8-browsers --push .
+docker build --file 3.11/Dockerfile -t cimg/python:3.11.6 -t cimg/python:3.11 --platform linux/amd64 .
+docker build --file 3.11/node/Dockerfile -t cimg/python:3.11.6-node -t cimg/python:3.11-node --platform linux/amd64 .
+docker build --file 3.11/browsers/Dockerfile -t cimg/python:3.11.6-browsers -t cimg/python:3.11-browsers --platform linux/amd64 .
+docker build --file 3.12/Dockerfile -t cimg/python:3.12.0 -t cimg/python:3.12 --platform linux/amd64 .
+docker build --file 3.12/node/Dockerfile -t cimg/python:3.12.0-node -t cimg/python:3.12-node --platform linux/amd64 .
+docker build --file 3.12/browsers/Dockerfile -t cimg/python:3.12.0-browsers -t cimg/python:3.12-browsers --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-set -eo pipefail
+docker push cimg/python:3.11
+docker push cimg/python:3.11.6
+docker push cimg/python:3.11-node
+docker push cimg/python:3.11.6-node
+docker push cimg/python:3.11-browsers
+docker push cimg/python:3.11.6-browsers
+docker push cimg/python:3.12
+docker push cimg/python:3.12.0
+docker push cimg/python:3.12-node
+docker push cimg/python:3.12.0-node
+docker push cimg/python:3.12-browsers
+docker push cimg/python:3.12.0-browsers
