3c45f442ee6e2ed3b3a5d61b13e1df32464ceffe
Bryan Johnson <bjohnso5@gmail.com>
2024-10-16 14:28:35 -0400

    Publish v3.13.0. [release] (#254)
    Publish v3.13.0.[release]

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.12/Dockerfile -t cimg/python:3.12.7 -t cimg/python:3.12 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 3.12/node/Dockerfile -t cimg/python:3.12.7-node -t cimg/python:3.12-node --push .
-docker buildx build --platform=linux/amd64 --file 3.12/browsers/Dockerfile -t cimg/python:3.12.7-browsers -t cimg/python:3.12-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 3.13/Dockerfile -t cimg/python:3.13 -t cimg/python:3.13 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 3.13/node/Dockerfile -t cimg/python:3.13-node -t cimg/python:3.13-node --push .
+docker buildx build --platform=linux/amd64 --file 3.13/browsers/Dockerfile -t cimg/python:3.13-browsers -t cimg/python:3.13-browsers --push .
